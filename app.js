40d3b3acd29b2e253f8c6d73c23b3021ea364751
JeffAllison <itsjallison@gmail.com>
2022-03-17 11:39:02 -0700

    added DOM elements and let state

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
 
+const formEl = document.querySelector('form');
+const goblinsListEl = document.querySelector('goblins-list');
+const goblinsDefeatedEl = document.querySelector('goblins-defeated');
+const playerStatsEl = document.querySelector('player-stats');
+
 // let state
+
+let playerStats = 10;
+let goblinsDefeated = 0;
+const goblins = [
+  { name: 'Guts', hp: 3 },
+  { name: 'Uggs the Goblin', hp: 2 }
+];
 
 // set event listeners 
   // get user input
-  // use user input to update state 
+  // use user input to update state
   // update DOM to reflect the new state
